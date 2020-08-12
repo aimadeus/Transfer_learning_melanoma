4e47cfc17a6830b00fbb2dc5861268e8f6370838
EmmaRocheteau <ecr38@cam.ac.uk>
2020-08-12 19:43:58 +0100

    set up framework

---
+++ model.py
@@ -0,0 +1,5 @@
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Activation, BatchNormalization, Dropout
+from keras.optimizers import Adam
+
+model = Sequential()
